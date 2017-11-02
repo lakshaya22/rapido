f470180a38cfb8ea50473db9bc184f7aa624499b
Lakshaya <lakshaya@byg.co.in>
2017-11-02 15:19:16 +0530

    karma and phantom removed

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var karma = require('karma').server;
+// var karma = require('karma').server;
 var argv = require('yargs').argv;
 var $ = require('gulp-load-plugins')();
 var port =process.env.PORT;
@@ -100,12 +100,12 @@ gulp.task('serve', ['wiredep', 'connect', 'fonts', 'watch'], function() {
   }
 });
 
-gulp.task('test', function(done) {
-  karma.start({
-    configFile: __dirname + '/test/karma.conf.js',
-    singleRun: true
-  }, done);
-});
+// gulp.task('test', function(done) {
+//   karma.start({
+//     configFile: __dirname + '/test/karma.conf.js',
+//     singleRun: true
+//   }, done);
+// });
 
 // inject bower components
 gulp.task('wiredep', function() {
